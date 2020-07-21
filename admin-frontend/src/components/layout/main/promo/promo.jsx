03eb3a3b0a07e4689d176470acf0807553b603fc
sagarmachine <sagarmonkey800@gmail.com>
2020-07-21 08:30:00 +0530

    promo component added

---
+++ admin-frontend/src/components/layout/main/promo/promo.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+class Promo extends Component {
+    render() {
+        return (
+            <div>
+                {"PROMO"}
+            </div>
+        )
+    }
+}
+
+
+
+
+export default Promo;
