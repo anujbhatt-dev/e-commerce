d3847cc4fca6d59656251f0dc171702d1136f9f4
sagarmachine <sagarmonkey800@gmail.com>
2020-07-25 18:23:59 +0530

    forgotpassword route added

---
+++ frontend-e-commerce/src/components/layout/forgot-password/forgot-password.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+
+class ForgotPassword extends Component {
+
+
+    state={
+        uuid:null,
+        email:null,
+       password:null,
+    }
+
+
+    componentDidMount=()=>{
+
+        
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                
+
+            </div>
+        )
+    }
+}
+
+
+export default  ForgotPassword;
