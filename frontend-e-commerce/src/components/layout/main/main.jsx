56397f5a5239a4a08510eaea7428a02684046138
anuj bhatt <anujbhatt023@gmail.com>
2021-01-04 14:36:06 +0530

    exim fix

--- frontend-e-commerce/src/components/layout/main/main.jsx
+++ frontend-e-commerce/src/components/layout/main/main.jsx
@@ -61,6 +61,18 @@ class Main extends Component{
          selectedCategoryHandler={this.selectedCategoryHandler}/>
          <BigImage/>
          {/* <div style="width:100%;height:0px;position:relative;padding-bottom:100.000%;"><iframe src="https://streamable.com/e/f1pcwf?autoplay=1&nocontrols=1" frameborder="0" width="100%" height="100%" allowfullscreen allow="autoplay" style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div> */}
+         <div className="sustainibility">
+               <div className="sustainibility__heading">sustainibility</div>
+               <div className="sustainibility__content">
+                  <span>#theconsciousclan </span>
+                  <span>Sustainably sourced organic cotton</span>
+                  <span>Organic cotton is better. Period. For the farmers, the environment, and more importantly, for you. </span>
+                  <span>Plastic-free packaging </span>
+                  <span>We use upcycled newspaper and kraft paper boxes to wrap your t-shirts with plastic-free tape for guilt-free indulgence. </span>
+                  <span>Bonus! Wheatgrass seeds</span>
+                  <span>The grass that keeps on giving - nutritious, full of antioxidants, anti-inflammatory, and easy to add to your diet.</span>
+               </div>
+         </div>
          <Products
          cart={this.props.cart}
          cartHandler={this.props.cartHandler}
@@ -71,6 +83,19 @@ class Main extends Component{
          sortBy={this.state.sortBy}
          selectedCategory={this.state.selectedCategory}
          sortByHandler={this.sortByHandler}/>
+         <div className="sustainibility">
+               <div className="sustainibility__heading">About Us</div>
+               <div className="sustainibility__content">
+
+                     <span>Inner Clan celebrates the human experience - of being creative and wearing</span>
+                     <span>your heart on your sleeve - while being responsible towards the planet.</span>
+                     <span>We are a sustainability-conscious fashion brand and pride ourselves on</span>
+                     <span>producing premium quality t-shirts in 100% organic</span>
+                     <span>cotton inspired by contemporary urban cult</span>
+
+
+               </div>
+         </div>
        </>
     )
   }
